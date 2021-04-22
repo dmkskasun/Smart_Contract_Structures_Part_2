4d2ca7f00c63036d2324ec2e947f355d01f18ad8
dmkskasun <dmkskasun@user.noreply.github.com>
2021-04-22 15:46:49 -0700

    sisx

---
+++ MyContract2first.sol
@@ -0,0 +1,10 @@
+pragma solidity 0.5.1;
+
+contract MyContract {
+    string public stringvalue = "KasunValue";
+    bool public kasunBool = false;
+    int public kasunInt = -1;
+    uint public kasunUint = 12;
+    uint256 public kasunUint256 = 254666;
+    
+}
